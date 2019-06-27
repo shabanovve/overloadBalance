94be33a70db761c61d641e73b9ca4ebcc6b0af9b
Владимир Шабанов <v.shabanov@swemel.ru>
2019-06-27 16:43:00 +0400

    Add client docker container

---
+++ docker/client/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -x
+
+JMX_PORT=9010
+HOST="0.0.0.0"
+
+java \
+  -Dsun.management.jmxremote.level=FINEST \
+  -Dsun.management.jmxremote.handlers=java.util.logging.ConsoleHandler \
+  -Djava.util.logging.ConsoleHandler.level=FINEST \
+  -Dcom.sun.management.jmxremote.local.only=false \
+  -Dcom.sun.management.jmxremote.ssl=false \
+  -Dcom.sun.management.jmxremote.authenticate=false \
+  -Dcom.sun.management.jmxremote.port=$JMX_PORT \
+  -Dcom.sun.management.jmxremote.rmi.port=$JMX_PORT \
+  -Dcom.sun.management.jmxremote.host=$HOST \
+  -Djava.rmi.server.hostname=$HOST \
+  -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 \
+  -jar /opt/app/client.jar
+
