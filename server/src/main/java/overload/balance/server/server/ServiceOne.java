04f4dbf4286dda6339072c498ac9564c4c17916f
Владимир Шабанов <v.shabanov@swemel.ru>
2019-06-27 19:00:30 +0400

    Make delay decrement

--- server/src/main/java/overload/balance/server/server/ServiceOne.java
+++ server/src/main/java/overload/balance/server/server/ServiceOne.java
@@ -2,19 +2,41 @@ package overload.balance.server.server;
 
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+
 
 @Service
 public class ServiceOne {
 
     private int delay;
+    private Instant last = Instant.now();
 
     public int getDelay() {
-        int result = delay++;
+        incrementDelay();
+        decrimentDelay();
+
+        int result = delay;
         try {
-            Thread.sleep(delay);
+            Thread.sleep(result);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
         return result;
+    }
+
+    private void decrimentDelay() {
+        Instant current = Instant.now();
+        long diff = current.getEpochSecond() - last.getEpochSecond();
+        int dec = (int) (diff * 3);
+        if (delay > 0 && dec > 0) {
+            delay = (delay - dec) < 0 ? 0 : delay - dec;
+        }
+        last = current;
+    }
+
+    private void incrementDelay() {
+        if (delay < 1000) {
+            delay = delay + 20;
+        }
     }
 }
