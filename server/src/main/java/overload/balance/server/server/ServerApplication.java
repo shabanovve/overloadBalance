3f21fe6c8bd5553d26da6305f97a73c3529b4548
Владимир Шабанов <v.shabanov@swemel.ru>
2019-07-03 15:24:29 +0400

    Remove @EnableHystrix

--- server/src/main/java/overload/balance/server/server/ServerApplication.java
+++ server/src/main/java/overload/balance/server/server/ServerApplication.java
@@ -3,10 +3,8 @@ package overload.balance.server.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @EnableDiscoveryClient
-@EnableHystrix
 @SpringBootApplication
 public class ServerApplication {
 
