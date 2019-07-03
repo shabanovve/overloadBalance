7ef90e76f723ebf4a1639e974fb5c1d61d06f0af
Владимир Шабанов <v.shabanov@swemel.ru>
2019-07-03 15:42:18 +0400

    Activate @EnableHystrixDashboard

--- client/src/main/java/overload/balance/client/client/ClientApplication.java
+++ client/src/main/java/overload/balance/client/client/ClientApplication.java
@@ -2,12 +2,18 @@ package overload.balance.client.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableScheduling
+@EnableCircuitBreaker
+@EnableHystrix
+@EnableHystrixDashboard
 public class ClientApplication {
 
     public static void main(String[] args) {
